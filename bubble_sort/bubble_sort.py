543916b64d6bb389ae7a12a4b7cd40a72d34e284
Yukih <158501974+Y-u-k-i-h@users.noreply.github.com>
2025-06-20 11:41:59 +0300

    First submit

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,16 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    # For loop to iterate through the list
+    for i in range (len(unsorted_list) - 1):
+        # Iterate through the list again for each element
+        for j in range (len(unsorted_list) - i - 1):
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                # Swap the elements if the current is greater than the next
+                temp_element = unsorted_list[j]
+                unsorted_list[j] = unsorted_list[j + 1]
+                unsorted_list[j + 1] = temp_element
+    return unsorted_list
+
+# Something I learnt from C is that you can swap two variables without using a temporary variable by using a bitwise XOR operation.
+# unsorted_list[j] = unsorted_list[j] ^ unsorted_list[j + 1]
+# unsorted_list[j + 1] = unsorted_list[j] ^ unsorted_list[j + 1]
+# unsorted_list[j] = unsorted_list[j] ^ unsorted_list[j + 1]
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,14 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    for i in range(len(arr) - 1):
+        # Assume the first element is the minimum
+        min_index = i
+        # Iterate through the array again to find the minimum element
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        # Swap the minimum element with the first element
+        temp_element = arr[min_index]
+        arr[min_index] = arr[i]
+        arr[i] = temp_element
+
+    return arr
